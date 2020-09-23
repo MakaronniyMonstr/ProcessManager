91fabdb8e88ab74b5d6521b89566f03d62114415
smirnyagina-vn <smirnyagina-vn@users.noreply.github.com>
2020-09-24 01:34:54 +0300

    UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Shape;
 import javafx.stage.Modality;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
