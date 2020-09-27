b1162866792344e1a1aad155ebe637b056c3e6d2
smirnyagina-vn <smirnyagina-vn@users.noreply.github.com>
2020-09-27 15:03:17 +0300

    UI - Changes for processes

--- src/sample/layouts/DialogController.java
+++ src/sample/layouts/DialogController.java
@@ -1,15 +1,20 @@
 package sample.layouts;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import sample.utils.processinfoloader.ProcessInfoLoader;
 import sample.utils.processinfoloader.PropertyProcessEntry;
+import sample.utils.processinfoloader.UtilTask;
 
-public class DialogController {
+public class DialogController implements ProcessInfoLoader.OnUtilTaskCompletedListener{
 
     @FXML
     private TextField intLevelField;
+    @FXML
+    private TextField privilegesField;
     @FXML
     private TextField fileOwnerField;
     @FXML
@@ -19,6 +24,8 @@ public class DialogController {
     private Stage dialogStage;
     private PropertyProcessEntry processEntry;
     private boolean okClicked = false;
+
+    public DialogController() {ProcessInfoLoader.getInstance().addOnUtilTaskCompletedListener(this);}
 
 
     @FXML
@@ -33,10 +40,21 @@ public class DialogController {
     public void setProcessEntry(PropertyProcessEntry processEntry) {
         this.processEntry = processEntry;
 
-        //поменять на то, что должно быть
-        intLevelField.setText(processEntry.getBasePriority());
-        fileOwnerField.setText(processEntry.getOwnerName());
-        aclField.setText(processEntry.getProcessName());
+        //fileOwnerField.setText(processEntry.getOwnerName());
+        //aclField.setText(processEntry.getProcessName());
+        ProcessInfoLoader.getInstance().runNewTask(
+                new UtilTask(
+                        UtilTask.GET_PROCESS_INTEGRITY_LEVEL,
+                        processEntry.getProcessID()
+                )
+        );
+        ProcessInfoLoader.getInstance().runNewTask(
+                new UtilTask(
+                        UtilTask.GET_PROCESS_PRIVILEGES,
+                        processEntry.getProcessID()
+                )
+        );
+
     }
 
 
@@ -48,12 +66,24 @@ public class DialogController {
     private void handleOk() {
         if (isInputValid()) {
 
-            //поменять на то, что должно быть
-            /*
-            processEntry.setBasePriority(Integer.parseInt(intLevelField.getText()));
-            processEntry.setExePath(fileOwnerField.getText());
-            processEntry.setProcessName(aclField.getText());
-             */
+            ProcessInfoLoader.getInstance().runNewTask(
+                    new UtilTask(
+                            UtilTask.SET_PROCESS_INTEGRITY_LEVEL,
+                            processEntry.getProcessID()
+                    )
+            );
+            ProcessInfoLoader.getInstance().runNewTask
+                    (new UtilTask(
+                                    UtilTask.SET_PROCESS_PRIVILEGES,
+                                    processEntry.getProcessID()
+                            )
+                    );
+            /*ProcessInfoLoader.getInstance().runNewTask(
+                    new UtilTask(
+                            UtilTask.GET_MODULES_LIST,
+                            processEntry.getProcessID()
+                    )
+            );*/
 
             okClicked = true;
             dialogStage.close();
@@ -67,31 +97,26 @@ public class DialogController {
 
 
     private boolean isInputValid() {
+
         String errorMessage = "";
 
-        if (fileOwnerField.getText() == null || fileOwnerField.getText().length() == 0) {
-            errorMessage += "No valid file owner name!\n";
-        }
-        if (aclField.getText() == null || aclField.getText().length() == 0) {
-            errorMessage += "No valid ACL list!\n";
-        }
+        //if (fileOwnerField.getText() == null || fileOwnerField.getText().length() == 0) {
+        //    errorMessage += "No valid file owner name!\n";
+        //}
+        //if (aclField.getText() == null || aclField.getText().length() == 0) {
+        //    errorMessage += "No valid ACL list!\n";
+        //}
 
-        if (intLevelField.getText() == null || intLevelField.getText().length() == 0) {
-            errorMessage += "No valid postal code!\n";
-        } else {
-            // пытаемся преобразовать почтовый код в int.
-            try {
-                Integer.parseInt(intLevelField.getText());
-            } catch (NumberFormatException e) {
-                errorMessage += "No valid postal code (must be an integer)!\n";
-            }
+        String intLevel = intLevelField.getText();
+        if (intLevel == null || intLevel.length() == 0
+                             && (intLevel != "High" && intLevel != "Medium" && intLevel != "Untrusted")) {
+            errorMessage += "No valid level!\n";
         }
-
 
         if (errorMessage.length() == 0) {
             return true;
         } else {
-            // Показываем сообщение об ошибке.
+
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.initOwner(dialogStage);
             alert.setTitle("Invalid Fields");
@@ -102,5 +127,17 @@ public class DialogController {
 
             return false;
         }
+    }
+
+    @Override
+    public void onTaskCompleted(UtilTask task) {
+
+        Platform.runLater(() -> {
+
+            if (task.getCommand() == UtilTask.GET_PROCESS_INTEGRITY_LEVEL && task.getStringData() != null)
+                intLevelField.setText(task.getStringData());
+            if (task.getCommand() == UtilTask.GET_PROCESS_PRIVILEGES && task.getStringData() != null)
+                privilegesField.setText(task.getStringData());
+        });
     }
 }
